c47cd58bdb96a78759bd6c0060e0302ddbdf1be5
Mario56 <75806265+Mario56@users.noreply.github.com>
2022-07-01 17:23:18 -0400

    Agregamos /ranking

--- ranking/assets/scripts/main.js
+++ ranking/assets/scripts/main.js
@@ -12,7 +12,7 @@ links.forEach(function(link){
         content.classList.add("animate__animated")
 
         setTimeout(function() {
-            location.href = "/"
+            location.href = "/ranking"
         }, 600);
         return false;
     })
