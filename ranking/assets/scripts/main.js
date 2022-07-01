f322ac39beeca1f241fa3240f61dddf093ba1ea8
Mario56 <75806265+Mario56@users.noreply.github.com>
2022-07-01 17:21:06 -0400

    Subo una Ranking de Mejores Juegos

---
+++ ranking/assets/scripts/main.js
@@ -0,0 +1,19 @@
+let links = document.querySelectorAll(".close")
+
+links.forEach(function(link){
+    link.addEventListener("click", function(ev){
+        ev.preventDefault();
+        let content = document.querySelector(".content")
+
+        content.classList.remove("animate__zoomInDown")
+        content.classList.remove("animate__animated")
+
+        content.classList.add("animate__zoomOutDown")
+        content.classList.add("animate__animated")
+
+        setTimeout(function() {
+            location.href = "/"
+        }, 600);
+        return false;
+    })
+})
